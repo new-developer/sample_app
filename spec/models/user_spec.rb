677ac6bd4adbd3c5ffc7f3c99b4c72a783e71fdc
Your Name <your.email@example.com>
2014-02-09 08:47:09 +0200

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,11 +14,12 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
-    
+     
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
@@ -104,4 +105,10 @@ describe "with a password that's too short" do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  
+    describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
   end
